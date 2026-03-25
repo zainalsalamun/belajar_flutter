57304ea9a0b72c1559d4620ed5d9329c87625a64
zainalsalamun <zainalrtf@gmail.com>
2026-03-25 19:59:35 +0700

    feat: Implement User model and ApiService with UserRepository for CRUD operations

---
+++ lib/riverpod/section_7_mvvm/services/api_service.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../config/api_constants.dart';
+
+final apiServiceProvider = Provider((ref) => ApiService());
+
+class ApiService {
+  late final Dio dio;
+
+  ApiService() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': ApiConstants.apiKey,
+        },
+      ),
+    );
+
+    dio.interceptors.add(
+      LogInterceptor(
+        requestBody: true,
+        responseBody: true,
+        logPrint: (obj) => debugPrint(obj.toString()),
+      ),
+    );
+  }
+}
